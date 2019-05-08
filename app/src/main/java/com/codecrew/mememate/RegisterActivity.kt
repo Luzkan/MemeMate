aef50e36c8b59e89bcf777428a111f8f9564f336
karolszymonczyk <karolszymonczyk@gmail.com>
2019-05-08 17:09:11 +0200

    animations and logo added

--- app/src/main/java/com/codecrew/mememate/RegisterActivity.kt
+++ app/src/main/java/com/codecrew/mememate/RegisterActivity.kt
@@ -2,31 +2,67 @@ package com.codecrew.mememate
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.transition.TransitionManager
 import android.view.View
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
 
+    private val handler = Handler()
+
+    private val runnable = {
+        TransitionManager.beginDelayedTransition(lRoot)
+        lRegister.visibility = View.VISIBLE
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val email = etEmail.text.toString()
-        val password = etPassword.text.toString()
+//        val email = etEmail.text.toString()
+//        val password = etPassword.text.toString()
+
+        handler.postDelayed(runnable, 1500)
     }
 
-    fun bRegisterClick(view: View) {
+    fun bSubmitClick(view: View) {
+        if (bSubmit.tag == "register") {
+            startApp()
+        } else {
+            startApp()
+        }
+    }
+
+    private fun startApp() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
-
-        println("REGISTER R")
     }
 
-    fun tvLoginClick(view: View) {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+    fun tvChangeClick(view: View) {
+        if (bSubmit.tag == "register") {
+            setLoginPanel()
+        } else {
+            setRegisterPanel()
+        }
+    }
 
-        println("LOGIN R")
+    private fun setLoginPanel() {
+        TransitionManager.beginDelayedTransition(lRoot)
+        etUsername.visibility = View.GONE
+        etPasswordConfirm.visibility = View.GONE
+        tvChange.text = "New user? Sign up here!"
+        bSubmit.text = "LOG IN"
+        bSubmit.tag = "login"
+    }
+
+    private fun setRegisterPanel() {
+        TransitionManager.beginDelayedTransition(lRoot)
+        etUsername.visibility = View.VISIBLE
+        etPasswordConfirm.visibility = View.VISIBLE
+        tvChange.text = "Already have an account?"
+        bSubmit.text = "REGISTER"
+        bSubmit.tag = "register"
     }
 }
