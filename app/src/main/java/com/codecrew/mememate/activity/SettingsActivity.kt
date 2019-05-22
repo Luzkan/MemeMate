f072f66eb469c1f15895dfeffc13da19455a1cd1
pawelrubin <pawelrubin19@gmail.com>
2019-05-22 16:38:17 +0200

    Settings -> User account deletion.

--- app/src/main/java/com/codecrew/mememate/activity/SettingsActivity.kt
+++ app/src/main/java/com/codecrew/mememate/activity/SettingsActivity.kt
@@ -1,5 +1,7 @@
 package com.codecrew.mememate.activity
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
@@ -52,4 +54,47 @@ class SettingsActivity : AppCompatActivity() {
                 }
         }
     }
+
+    fun deleteUser(view: View) {
+        delete_account_button.isEnabled = false
+
+        val alertDialog = this.let { settingsActivity ->
+            val builder = AlertDialog.Builder(settingsActivity)
+            builder.apply {
+                setPositiveButton(R.string.yes) { dialog, id ->
+                    // User clicked Yes button
+                    val user = FirebaseAuth.getInstance().currentUser
+                    user?.delete()
+                        ?.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Log.d("SETTINGS", "User account deleted.")
+                                Toast.makeText(this.context, "User account deleted.", Toast.LENGTH_SHORT).show()
+                                Intent(this.context, RegisterActivity::class.java).also {
+                                    it.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                                    startActivity(it)
+                                }
+                            } else {
+                                Log.d("SETTINGS", "User account deletion error.")
+                                Toast.makeText(this.context, "Something went wrong.", Toast.LENGTH_SHORT).show()
+                                delete_account_button.isEnabled = true
+                            }
+                        }
+                }
+                setNegativeButton(R.string.no) { dialog, id ->
+                    // User cancelled the dialog
+                    delete_account_button.isEnabled = true
+                }
+            }
+            // Set other dialog properties
+            builder.setMessage("Are you sure? This operation cannot be rewind!")
+            builder.setTitle("Delete my account")
+
+            // Create the AlertDialog
+            builder.create()
+        }
+
+        alertDialog.show()
+
+    }
+
 }
